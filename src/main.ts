f5160a047a0458db853306455f848cdeb980b8bf
Promochi0141 <130904564+Promochi0141@users.noreply.github.com>
2024-11-24 13:45:43 +0900

    一次デプロイ

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,39 @@
 import { Viewer } from '../lib/main';
 
+import './style.css';
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div id="turn">Test</div>
-  <div class="test">
-    <button id="previous">Previous</button>
-    <button id="next">Next</button>
-  </div>
-  <style>
-    @import url('https://unpkg.com/sanitize.css');
-    #app {
-      width: 100dvw;
-      height: 100dvh;
-      padding-block: 1rem;
-    }
-    #turn {
-      height: 90%;
-    }
-  </style>
+  <div id="slider"></div>
 `;
 
-const turnBook = new Viewer('#turn', { type: 'pdf', src: '../assets/14.pdf' });
+const turnBook = new Viewer('#turn', { type: 'pdf', src: '../assets/pamphlet_mobile.pdf' });
 
 turnBook.mount();
 
 document.querySelector('#previous')?.addEventListener('click', () => turnBook.previous());
 document.querySelector('#next')?.addEventListener('click', () => turnBook.next());
+
+const turn = document.getElementById('turn');
+
+if (turn) {
+  turn.addEventListener('click', (event) => {
+    const rect = turn.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+
+    if (x > rect.width / 2) {
+      turnBook.next();
+    } else if (x < rect.width / 2) {
+      turnBook.previous();
+    }
+  });
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+      turnBook.next();
+    } else if (event.key === 'ArrowLeft') {
+      turnBook.previous();
+    }
+  });
+
+}
